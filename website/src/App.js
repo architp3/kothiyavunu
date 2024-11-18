81e5c139aa85ed8b434b67ea8ddaf43a9ad900f0
archit 1403 <architpimple@gmail.com>
2024-11-18 00:40:51 -0800

    Code works

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.css';
+import './output.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500"> Hello </h1>
+    </div>
+  );
+}
+
+export default App;
